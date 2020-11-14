18ac7285726cc1c4aec407c90076fd794edb0489
StijnWillemen <60694521+StijnWillemen@users.noreply.github.com>
2020-11-14 18:13:25 +0100

    UpdateStartRegion

--- Mobile-Groepswerk/stadAntwerpenInfo/components/MapsScreen.js
+++ Mobile-Groepswerk/stadAntwerpenInfo/components/MapsScreen.js
@@ -10,7 +10,10 @@ export default MapsScreen = ({navigation}) => {
 
     return (
       <View style={styles.containerMap}>
-        <MapView style={styles.mapStyle} >
+        <MapView style={styles.mapStyle} region={{latitude:51.228493,
+           longitude:4.404578,
+           longitudeDelta:0.12874,
+           latitudeDelta:0.04888}} >
   
         </MapView>
       </View>
