362ff2c00fdabec122e37d30c4954247dae97abc
Brent_Dardenne <s109305@ap.be>
2020-11-20 16:31:17 +0100

    Favorieten 3

--- Mobile-Groepswerk/stadAntwerpenInfo/components/ListDetails.js
+++ Mobile-Groepswerk/stadAntwerpenInfo/components/ListDetails.js
@@ -5,19 +5,19 @@ import AsyncStorage from '@react-native-community/async-storage';
 import {ListItem} from 'react-native-elements'
 import { navigation} from '@react-navigation/native';
 
-//Pagina List
+//Pagina List: Stijn Pas op als je hier iets aanpast dit was egt iritant om te maken laat alleen fixen
 export default ListDetails = (props) => {
-  const [buttonText, setButtonText] = useState("Voeg toe aan favorieten");
-  const[favs,setFav]=useState(false) //
+  const [buttonText, setButtonText] = useState("Voeg toe aan favorieten"); // Voor de text op de knop te tonen van favorieten
+  const[favs,setFav]=useState(false) // om van true naar false te gaan false is als het niet gesaven is true als het wel gesavend is. Standard false dit om als je het programma zou opstarten nog niks gesaved is
   const[saveId, setSaveId] = useState()
   
   
   const storeFav = async (value) => { //saved the value
     try {
-      await AsyncStorage.setItem(`@${value.properties.naam}`, JSON.stringify(value))
-      console.log('saved ' + value.properties.naam)
+      await AsyncStorage.setItem(`@${value.properties.naam}`, JSON.stringify(value)) // als key geven de de naam van het zwembad mee en geven we als data de heele api zwambad data
+      console.log('saved ' + value.properties.naam) // om te kijken of dat hij saven
 
-      setButtonText("Verwijder uit favorieten")
+      setButtonText("Verwijderen uit favorieten") // voor de button te zetten op verijder uit favorieten
     } catch (e) {
         // saving error
       }
@@ -26,9 +26,9 @@ export default ListDetails = (props) => {
     const deleteFav = async (value) => { // delete the value
       try {
         console.log('ik ga verwijderen')
-        await AsyncStorage.removeItem(`@${value.properties.naam}`)
+        await AsyncStorage.removeItem(`@${value.properties.naam}`) //als key gebruiken we de naam
         console.log('delete ' + value.properties.naam)
-        setButtonText("Voeg toe aan favorieten")
+        setButtonText("toevoegen") // hier veranderen we de text van de button op toevoegen
       } catch(e) {
 
       }
@@ -44,9 +44,9 @@ export default ListDetails = (props) => {
   
         const loaded = await AsyncStorage.getItem(`@${value.properties.naam}`)
         console.log(loaded)
-        if(loaded !== null) {
-          setButtonText("Verwijder uit favorieten")
-          setFav(true)
+        if(loaded !== null) { //Als loaded niet null is dan verwijder favorieten
+          setButtonText("Verwijder uit favorieten") //Zet de text naar verwijder favorieten
+          setFav(true) // zet state setFav naar true=DeleteFav
       }
       
       } catch(e) {
@@ -68,7 +68,7 @@ export default ListDetails = (props) => {
         <Text style={styles.htext}>Publiek:</Text>
         <Text style={styles.text}>{props.route.params.properties.publiek}</Text>
         
-        <Button title={buttonText} onPress={()=>{ favs ? deleteFav(props.route.params) : storeFav(props.route.params); setFav(!favs)}}/>
+        <Button title={buttonText} onPress={()=>{ favs ? deleteFav(props.route.params) : storeFav(props.route.params); setFav(!favs)}}/> 
     
       </View>
 
--- Mobile-Groepswerk/stadAntwerpenInfo/components/ListFavorite.js
+++ Mobile-Groepswerk/stadAntwerpenInfo/components/ListFavorite.js
@@ -1,34 +1,44 @@
 import React ,{ useEffect, useState } from 'react';
 import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { FlatList, TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
+import {ListItem} from 'react-native-elements'
 import { navigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default Favorite = (props) => {
-  const[storedData, setStoredData]=useState([]) //
-  
+  const[storedData, setStoredData]=useState() //
+  let favoriteArray = [];
   let allData = []
   allData = props.publicSwim;
   //console.log(allData);
 
   //setStoredData()
-  const id = () => {
-    
+  const id = async () =>  {
+    let arraytest = [];
+    //let favoriteArray = []; 
     allData.forEach( async (location) => {
-      const value = await AsyncStorage.getItem(`@${location.properties.naam}`)
-      if(value !== null) {
-        setStoredData(storedData.push(location))
-        //console.log(storedData);
+      const value = await AsyncStorage.getItem(`@${location.properties.naam}`) // We matche de waarde tot de naam overeenkomt als die overeenkomt wilt het zeggen dat we die saved hebben
+      //console.log('value ' + value)
+      if(value !== null) { // als de value groter is dan null word 
+        //setStoredData(value)
+        arraytest.push(value);
+        setStoredData([...arraytest])
+        //console.log('waarde in de if ' + value + '\n') // dit is juist geeft twee waarden
+        //favoriteArray.push(value)
+        //console.log('value is not null = ' + storedData);
       }
         
-        //console.log(value);
-    })
-    
+    });
+      
+        //console.log('test2' + favoriteArray);
+        //setStoredData([...arraytest]);
   }
   
-  id(props.publicSwim);
-  console.log(storedData);
+  id();
+  
+  //console.log('hallo ' + storedData.feature.properties.id);
+  //console.log('Hell' + storedData + '\n');
 
 
   //AsyncStorage.getItem(item.name);
@@ -37,12 +47,23 @@ export default Favorite = (props) => {
   
   //let addData = props.addFavorite
   //let removeData = props.removeFavorte
-  
+  //console.log(storedData);
     return (
-      
-      <View style={styles.containerMap}>
-        <Text></Text>
-        <Text></Text>
+      <View >
+        <FlatList
+        data={storedData != undefined}
+        //key={item => item.OBJECTID}
+        //keyExtractor={properties.id}
+        renderItem={item=>(
+        <ListItem >
+          <ListItem.Content >
+        <ListItem.Title >{item.naam}</ListItem.Title>
+            <ListItem.Subtitle ></ListItem.Subtitle> 
+          </ListItem.Content>
+        </ListItem>)}>
+
+        </FlatList>
+        
       </View>
     )
     
